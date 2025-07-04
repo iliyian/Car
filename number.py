a99f7c7c7c2b4ac70a434677c2b598aee13c215d
时光机 <ywhyfy@163.com>
2025-07-04 09:23:03 +0800

    科目二

--- number.py
+++ number.py
@@ -5,4 +5,4 @@ import numpy as np
 for num in range(10):
     img = np.zeros((100, 100), dtype=np.uint8) + 255  # 白底
     cv2.putText(img, str(num), (30, 70), cv2.FONT_HERSHEY_SIMPLEX, 2, 0, 3)  # 黑字
-    cv2.imwrite(f"digits/{num}.png", img)  # 保存到digits文件夹
+    cv2.imwrite(f"digits/{num}.png", img)  # 保存到digits文件夹1
